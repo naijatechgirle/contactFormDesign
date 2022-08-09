7b57907f532a5c2ca61f2bd058ab84e550d249db
naijatechgirle <ruthiekelly38@gmail.com>
2022-08-09 14:42:49 +0100

    updated changes

--- designOne/script.js
+++ designOne/script.js
@@ -1,20 +1,3 @@
-// 'use strict';
-
-// let forms = document.getElementsByClassName('needs-validation');
-// (function() {
-//     window.addEventListener('load', function() {
-//         let validation = Array.prototype.filter.call(forms, function(form) {
-//         form.addEventListener('submit', function(event) {
-//             if (form.checkValidity() === false) {
-//             event.preventDefault();
-//             event.stopPropagation();
-//             }
-//             form.classList.add('was-validated');
-//         }, false);
-//         });
-//     }, false);
-// })();
-
 (function() {
     'use strict';
     window.addEventListener('load', function() {
